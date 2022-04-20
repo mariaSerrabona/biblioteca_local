a587a32743df8769c398e265d5fec381f673c502
mariaSerrabona <83339874+mariaSerrabona@users.noreply.github.com>
2022-04-20 13:09:45 +0200

    completando la clase admin

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Author, Genre, Book, BookInstance, Lenguage
 
 # Register your models here.
+class AuthorAdmin(admin.ModelAdmin):
+    #la definimos con los atributos que caracterizan a un autor
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    pass
+
+#importamos todas las clases que hemos creado antes en el models.py y las de las clases de arriba
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(BookInstance)
+admin.site.register(Lenguage)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Book)
+admin.site.register(BookInstance)
