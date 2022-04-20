7a8175342002c5ef9137f2280d2ed12a956c98d9
mariaSerrabona <83339874+mariaSerrabona@users.noreply.github.com>
2022-04-20 12:15:58 +0200

    corrigiendo y completando models.py de la app

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -1,39 +1,49 @@
 from django.db import models
+from django.urls import reverse #Used to generate URLs by reversing the URL patterns
+
 
 # Create your models here.
-
-class MyModelName(models.Model):
+class Genre(models.Model):
     """
-    Una clase típica definiendo un modelo, derivado desde la clase Model.
+    Modelo que representa un género literario (p. ej. ciencia ficción, poesía, etc.).
     """
-
-    # Cadena de caracteres, tiene etiqueta de texto de ayuda (help_txt)
-    my_field_name = models.CharField(max_length=20, help_text="Enter field documentation")
-    ...
-
-    # Metadata, controla el orden pro defecrto de los registrosque se devuelve cuando se consulta el tipo de modelo.
-    #los libros irán ordenados alfabéticamente
-    class Meta:
-        ordering = ["-my_field_name"]
-
-    # Métodos
-    def get_absolute_url(self):
-        """
-        Devuelve la url para acceder a una instancia particular de MyModelName.
-        """
-        return reverse('model-detail-view', args=[str(self.id)])
+    name = models.CharField(max_length=200, help_text="Ingrese el nombre del género (p. ej. Ciencia Ficción, Poesía Francesa etc.)")
 
     def __str__(self):
         """
-        Cadena para representar el objeto MyModelName (en el sitio de Admin, etc.)
+        Cadena que representa a la instancia particular del modelo (p. ej. en el sitio de Administración)
         """
-        return self.field_name
+        return self.name
 
 
+class Book(models.Model):
+    """
+    Modelo que representa un libro (pero no un Ejemplar específico).
+    """
+
+    title = models.CharField(max_length=200)
+
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+    # ForeignKey, ya que un libro tiene un solo autor, pero el mismo autor puede haber escrito muchos libros.
+    # 'Author' es un string, en vez de un objeto, porque la clase Author aún no ha sido declarada.
+
+    summary = models.TextField(max_length=1000, help_text="Ingrese una breve descripción del libro")
+
+    isbn = models.CharField('ISBN',max_length=13, help_text='13 Caracteres <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
+
+    genre = models.ManyToManyField(Genre, help_text="Seleccione un genero para este libro")
+    # ManyToManyField, porque un género puede contener muchos libros y un libro puede cubrir varios géneros.
+    # La clase Genre ya ha sido definida, entonces podemos especificar el objeto arriba.
+
+    def __str__(self):
+        """
+        String que representa al objeto Book
+        """
+        return self.title
 
 
-# Creación de un nuevo registro usando el constructor del modelo.
-a_record = MyModelName(my_field_name="Instancia #1")
-
-# Guardar el objeto en la base de datos.
-a_record.save()
+    def get_absolute_url(self):
+        """
+        Devuelve el URL a una instancia particular de Book
+        """
+        return reverse('book-detail', args=[str(self.id)])
