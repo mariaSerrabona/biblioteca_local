f08fdcdbb47884d9485bd863c090145c0cfc62ea
mariaSerrabona <83339874+mariaSerrabona@users.noreply.github.com>
2022-04-22 08:53:19 +0200

    generando book_detail.html

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -31,3 +31,7 @@ class BookListView(generic.ListView):
     context_object_name = 'my_book_list'   # your own name for the list as a template variable
     queryset = Book.objects.filter(title__icontains='war')[:5] # Get 5 books containing the title war
     template_name = 'books/my_arbitrary_template_name_list.html'  # Specify your own template name/location
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
