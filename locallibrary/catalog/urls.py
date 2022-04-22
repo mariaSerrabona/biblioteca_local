80d78f64eb016bbb48cb280e639f297828cc3de6
mariaSerrabona <83339874+mariaSerrabona@users.noreply.github.com>
2022-04-22 09:22:17 +0200

    actualizando url que si no no  carga

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -17,5 +17,8 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^books/$', views.BookListView.as_view(), name='books'),
     url(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
+    path('authors/', views.AuthorListView.as_view(), name='authors'),
+    path('author/<int:pk>',
+        views.AuthorDetailView.as_view(), name='author-detail'),
 
 ]
