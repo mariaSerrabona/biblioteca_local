30ac0a7f275a335cf70e9f6dced62ffdcb8baa67
mariaSerrabona <83339874+mariaSerrabona@users.noreply.github.com>
2022-04-22 10:31:13 +0200

    redireccionamiento cuando entras como usuario

--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -114,6 +114,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+#redireccionamiento cuando entras como usuario
+LOGIN_REDIRECT_URL = '/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
