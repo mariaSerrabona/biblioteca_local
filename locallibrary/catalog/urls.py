3cf6505972765ad28535dae6589f3acdf21f3433
mariaSerrabona <83339874+mariaSerrabona@users.noreply.github.com>
2022-04-22 10:28:08 +0200

    equivocación en las url

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -19,9 +19,5 @@ urlpatterns = [
     path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
     path('authors/', views.AuthorListView.as_view(), name='authors'),
     path('author/<int:pk>',
-        views.AuthorDetailView.as_view(), name='author-detail'),
-]
+        views.AuthorDetailView.as_view(), name='author-detail'),]
 
-urlpatterns += [
-    path('accounts/', include('django.contrib.auth.urls')),
-]
