7ed2ceb6120a83930eec23db7dcc824f5a853efb
mariaSerrabona <83339874+mariaSerrabona@users.noreply.github.com>
2022-04-22 08:23:35 +0200

    siguiendo con los pasos

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -5,6 +5,9 @@ from django.conf.urls import url
 from . import views
 
 
+# ^ inicio de cadena y $  fin de cadena
+
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^books/$', views.BookListView.as_view(), name='books'),
 ]
