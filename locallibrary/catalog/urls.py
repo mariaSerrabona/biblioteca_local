cd3191a79eb59c2dfa33227eb9428eac27bfc79b
mariaSerrabona <83339874+mariaSerrabona@users.noreply.github.com>
2022-04-22 08:47:38 +0200

    añadiendo una url

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -5,9 +5,17 @@ from django.conf.urls import url
 from . import views
 
 
+##  ¿QUÉ SIGNIFICAN LOS CARACTERES DE LAS URL?
+
 # ^ inicio de cadena y $  fin de cadena
+# ? captura el patron como una variable con nombre
+# /d representa un dígito
+#r'^book/(?P<pk>\d+)$' camputa todos los dígitos y los envía como una cadena
+
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^books/$', views.BookListView.as_view(), name='books'),
+    url(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
+
 ]
