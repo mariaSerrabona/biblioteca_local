4dc3a1f54a2af533dadcd5226bc13600e76c8758
mariaSerrabona <83339874+mariaSerrabona@users.noreply.github.com>
2022-04-23 14:34:50 +0200

    completando las fechasde devolucion de los libros

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     path('author/<int:pk>',
         views.AuthorDetailView.as_view(), name='author-detail'),
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
 
 ]
 
