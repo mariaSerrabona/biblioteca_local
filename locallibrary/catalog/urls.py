bd7794e20ce79fcf869f4ca37b292f3808705e1b
mariaSerrabona <83339874+mariaSerrabona@users.noreply.github.com>
2022-04-23 18:05:26 +0200

    url libros

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -28,11 +28,17 @@ urlpatterns = [
 ]
 
 
-#url con lo deferente a la edición de autores
+#url con lo referente a la edición de autores
 urlpatterns += [
     path('authors/create/', views.AuthorCreate.as_view(), name='author-create'),
     path('authors/<int:pk>/update/', views.AuthorUpdate.as_view(), name='author-update'),
     path('authors/<int:pk>/delete/', views.AuthorDelete.as_view(), name='author-delete'),
 ]
 
+#url con lo referente a la edición de libros
+urlpatterns += [
+    path('book/create/', views.BookCreate.as_view(), name='book-create'),
+    path('book/<int:pk>/update/', views.BookUpdate.as_view(), name='book-update'),
+    path('book/<int:pk>/delete/', views.BookDelete.as_view(), name='book-delete'),
+]
 
