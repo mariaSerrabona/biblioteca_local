5f38b104029a456073831351eef8942ce490ad73
mariaSerrabona <83339874+mariaSerrabona@users.noreply.github.com>
2022-04-24 10:09:23 +0200

    completando test.py

--- locallibrary/catalog/tests/test_models.py
+++ locallibrary/catalog/tests/test_models.py
@@ -6,3 +6,29 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class YourTestClass(TestCase):
+
+    #para el comienzo de la ejecución. obj que no se modifican en ningún momento de la prueba.
+    @classmethod
+    def setUpTestData(cls):
+        print("setUpTestData: Run once to set up non-modified data for all class methods.")
+        pass
+
+    #cualquier obj que oouede ser modificado durante la prueba
+    def setUp(self):
+        print("setUp: Run once for every test method to setup clean data.")
+        pass
+
+    #assert comprobar si las condiciones son verdaderas o falsas.
+    def test_false_is_false(self):
+        print("Method: test_false_is_false.")
+        self.assertFalse(False)
+
+    def test_false_is_true(self):
+        print("Method: test_false_is_true.")
+        self.assertTrue(False)
+
+    def test_one_plus_one_equals_two(self):
+        print("Method: test_one_plus_one_equals_two.")
+        self.assertEqual(1 + 1, 2)
