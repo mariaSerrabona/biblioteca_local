7edde86388bedebf3caa068b8e07856181d423fe
mariaSerrabona <83339874+mariaSerrabona@users.noreply.github.com>
2022-04-24 10:05:16 +0200

    creando tests para probar web

---
+++ locallibrary/catalog/tests/test_models.py
@@ -0,0 +1,8 @@
+# se genera la carpeta de los test
+
+#cuando se quiere probar una web, cuando es sencilla, es fácil y rápido de probar, pero cunado la web va teniendo mayo número de widgeds, se puede tardar mucho tiempo
+#por ello, django nos ofrece la posibilidad de poder porbar nuestra página web para así ver que estaá todo correcto
+
+from django.test import TestCase
+
+# Create your tests here.
