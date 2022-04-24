c46e695f2af0f1bab96662b95eb3f22d8c148277
mariaSerrabona <83339874+mariaSerrabona@users.noreply.github.com>
2022-04-24 16:06:57 +0200

    organizando ficheros

--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 DEBUG = False
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['local-library-serrabona.herokuapp.com','127.0.0.1']
 
 
 # Application definition
