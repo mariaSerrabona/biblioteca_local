ee833690edeb22dbeef5c19e6657f20f843bec5c
mariaSerrabona <83339874+mariaSerrabona@users.noreply.github.com>
2022-04-24 10:27:38 +0200

    completando test models de autor

--- locallibrary/catalog/tests/test_models.py
+++ locallibrary/catalog/tests/test_models.py
@@ -4,31 +4,46 @@
 #por ello, django nos ofrece la posibilidad de poder porbar nuestra página web para así ver que estaá todo correcto
 
 from django.test import TestCase
+from catalog.models import Author
 
-# Create your tests here.
 
-class YourTestClass(TestCase):
+#hacemos las pruebas para ver si la parte de los autores etsá bien
+class AuthorModelTest(TestCase):
 
-    #para el comienzo de la ejecución. obj que no se modifican en ningún momento de la prueba.
+    #se genera un test por cada objeto que define un autor
     @classmethod
     def setUpTestData(cls):
-        print("setUpTestData: Run once to set up non-modified data for all class methods.")
-        pass
+        Author.objects.create(first_name='Big', last_name='Bob')
 
-    #cualquier obj que oouede ser modificado durante la prueba
-    def setUp(self):
-        print("setUp: Run once for every test method to setup clean data.")
-        pass
+    def test_first_name_label(self):
+        author=Author.objects.get(id=1)
+        #no podemos acceder directamente a los datos del autor, primero debemos acceder a su clase meta y usuarla para obtener info adicional
+        field_label = author._meta.get_field('first_name').verbose_name
+        #se emplea esta estructura para que el error salte exactamente donde está, haciendo más fácil el debugging
+        self.assertEquals(field_label,'first name')
 
-    #assert comprobar si las condiciones son verdaderas o falsas.
-    def test_false_is_false(self):
-        print("Method: test_false_is_false.")
-        self.assertFalse(False)
+    #siguiendo con los test, habrá que comprobar la fecha de nacimiento
+    def test_date_of_birth_label(self):
+        author = Author.objects.get(id=1)
+        field_label = author._meta.get_field('date_of_birth').verbose_name
+        self.assertEqual(field_label, 'date of birth')
 
-    def test_false_is_true(self):
-        print("Method: test_false_is_true.")
-        self.assertTrue(False)
+    def test_date_of_death_label(self):
+        author=Author.objects.get(id=1)
+        field_label = author._meta.get_field('date_of_death').verbose_name
+        self.assertEquals(field_label,'died')
 
-    def test_one_plus_one_equals_two(self):
-        print("Method: test_one_plus_one_equals_two.")
-        self.assertEqual(1 + 1, 2)
+    def test_first_name_max_length(self):
+        author=Author.objects.get(id=1)
+        max_length = author._meta.get_field('first_name').max_length
+        self.assertEquals(max_length,100)
+
+    def test_object_name_is_last_name_comma_first_name(self):
+        author=Author.objects.get(id=1)
+        expected_object_name = '%s, %s' % (author.last_name, author.first_name)
+        self.assertEquals(expected_object_name,str(author))
+
+    def test_get_absolute_url(self):
+        author=Author.objects.get(id=1)
+        #También fallará si la url no está bien configurada
+        self.assertEquals(author.get_absolute_url(),'/catalog/author/1')
